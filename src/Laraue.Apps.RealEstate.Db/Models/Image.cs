f749f509372fa425b9758216677c9efe955cd0b8
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2025-09-28 19:11:05 +0300

    add missing file

---
+++ src/Laraue.Apps.RealEstate.Db/Models/Image.cs
@@ -0,0 +1,27 @@
+﻿namespace Laraue.Apps.RealEstate.Db.Models;
+
+public sealed record Image
+{
+    public long Id { get; set; }
+    
+    /// <summary>
+    /// Rating of renovation if it exists.
+    /// </summary>
+    public double RenovationRating { get; init; }
+
+    /// <summary>
+    /// Image address.
+    /// </summary>
+    public string Url { get; init; } = default!;
+
+    public string? Description { get; init; }
+    public string[] Tags { get; init; } = [];
+    
+    public AdvertisementImageProcessState ProcessState { get; init; }
+}
+
+public enum AdvertisementImageProcessState
+{
+    None,
+    Processed,
+}
