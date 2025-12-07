2718866f4246a722e411ba1607dbcb081c4f7132
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2025-12-07 12:17:57 +0300

    add missing class

---
+++ src/Laraue.Apps.RealEstate.Crawling.Impl/SessionInterrupter.cs
@@ -0,0 +1,55 @@
+﻿using Laraue.Apps.RealEstate.Crawling.Abstractions.Contracts;
+using Laraue.Core.DateTime.Services.Abstractions;
+using Laraue.Crawling.Crawler;
+
+namespace Laraue.Apps.RealEstate.Crawling.Impl;
+
+public interface ISessionInterrupter
+{
+    void ThrowIfRequired(
+        HashSet<long> currentPageUpdatedAdvertisementIds,
+        HashSet<long> sessionUpdatedAdvertisementIds,
+        Advertisement[] crawledAdvertisements,
+        BaseCrawlerServiceOptions options);
+}
+
+public class SessionInterrupter(IDateTimeProvider dateTimeProvider) : ISessionInterrupter
+{
+    public void ThrowIfRequired(
+        HashSet<long> currentPageUpdatedAdvertisementIds,
+        HashSet<long> sessionUpdatedAdvertisementIds,
+        Advertisement[] crawledAdvertisements,
+        BaseCrawlerServiceOptions options)
+    {
+        // If some of advs were not updated need to check the reason.
+        if (currentPageUpdatedAdvertisementIds.Count != crawledAdvertisements.Length)
+        {
+            // Need to check adv identifiers. If all adv ids from this session, it just means
+            // that crawler was powered off for some time when new advs appeared.
+            // Else it means the crawler found items that were stored in previous sessions. 
+            if (!currentPageUpdatedAdvertisementIds.All(sessionUpdatedAdvertisementIds.Contains))
+            {
+                throw new SessionInterruptedException("Already parsed advertisements found");
+            }
+        }
+        
+        // If the system found advs that is too old (based on options), the session should be finished.
+        // It prevents the whole resource parsing at the first run.
+        if (!AreAllAdvertisementsActual(crawledAdvertisements, options))
+        {
+            throw new SessionInterruptedException("Too old advertisements found");
+        }
+    }
+    
+    private bool AreAllAdvertisementsActual(
+        IEnumerable<Advertisement> advertisements,
+        BaseCrawlerServiceOptions options)
+    {
+        var latestAdvertisementDate = advertisements.Max(x => x.UpdatedAt); // 2 days before | 1 day before -> 1 day before
+        
+        // now - 10 min before = - 10 min
+        // -10 min <= 15 day -> true
+        return dateTimeProvider.UtcNow - latestAdvertisementDate
+               <= options.MaxAdvertisementDateOffset;
+    }
+}
---
+++ tests/Laraue.Apps.RealEstate.UnitTests/SessionInterrupterTests.cs
@@ -0,0 +1,89 @@
+﻿using Laraue.Apps.RealEstate.Crawling.Abstractions.Contracts;
+using Laraue.Apps.RealEstate.Crawling.Impl;
+using Laraue.Apps.RealEstate.Crawling.Impl.Cian;
+using Laraue.Core.DateTime.Services.Abstractions;
+using Laraue.Crawling.Crawler;
+using Moq;
+
+namespace Laraue.Apps.RealEstate.UnitTests;
+
+public class SessionInterrupterTests
+{
+    private readonly SessionInterrupter _sessionInterrupter;
+    private readonly Mock<IDateTimeProvider> _dateTimeProvider;
+    private readonly CianCrawlerServiceOptions _options = new ()
+    {
+        MaxAdvertisementDateOffset = new TimeSpan(1, 0, 0),
+        Cities = [],
+    };
+
+    public SessionInterrupterTests()
+    {
+        _dateTimeProvider = new Mock<IDateTimeProvider>();
+        _sessionInterrupter = new SessionInterrupter(_dateTimeProvider.Object);
+    }
+
+    [Fact]
+    public void Session_ShouldBeInterrupted_WhenAdvertisementsFromPreviousSessionsFound()
+    {
+        var currentPageUpdatedAdvertisementIds = new HashSet<long> { 3, 4 };
+        var sessionUpdatedAdvertisementIds = new HashSet<long> { 1, 2 };
+        var crawledAdvertisements = Array.Empty<Advertisement>();
+        
+        var ex = Assert.Throws<SessionInterruptedException>(() => _sessionInterrupter.ThrowIfRequired(
+            currentPageUpdatedAdvertisementIds, 
+            sessionUpdatedAdvertisementIds,
+            crawledAdvertisements,
+            _options));
+        
+        Assert.Equal("Already parsed advertisements found", ex.Message);
+    }
+
+    [Fact]
+    public void Session_ShouldBeInterrupted_WhenTooOldAdvertisementsFound()
+    {
+        var currentPageUpdatedAdvertisementIds = new HashSet<long> { 2 };
+        var sessionUpdatedAdvertisementIds = new HashSet<long> { 1 };
+        var crawledAdvertisements = new []
+        {
+            new Advertisement
+            {
+                UpdatedAt = new DateTime(2020, 01, 01)
+            }
+        };
+        
+        _dateTimeProvider.Setup(x => x.UtcNow)
+            .Returns(new DateTime(2020, 01, 01, 02, 00, 00));
+        
+        var ex = Assert.Throws<SessionInterruptedException>(() => _sessionInterrupter.ThrowIfRequired(
+            currentPageUpdatedAdvertisementIds, 
+            sessionUpdatedAdvertisementIds,
+            crawledAdvertisements,
+            _options));
+        
+        Assert.Equal("Too old advertisements found", ex.Message);
+    }
+    
+    [Fact]
+    public void Session_ShouldNotBeInterrupted_WhenNoMatchInterruptionCriteria()
+    {
+        var currentPageUpdatedAdvertisementIds = new HashSet<long> { 2 };
+        var sessionUpdatedAdvertisementIds = new HashSet<long> { 1 };
+        var crawledAdvertisements = new []
+        {
+            new Advertisement
+            {
+                UpdatedAt = new DateTime(2020, 01, 01)
+            }
+        };
+        
+        _dateTimeProvider.Setup(x => x.UtcNow)
+            .Returns(new DateTime(2020, 01, 01, 00, 10, 00));
+
+        _sessionInterrupter.ThrowIfRequired(
+            currentPageUpdatedAdvertisementIds,
+            sessionUpdatedAdvertisementIds,
+            crawledAdvertisements,
+            _options);
+    }
+}
