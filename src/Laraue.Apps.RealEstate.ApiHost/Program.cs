449f19deecabfbc4abf1abab3a23cccff41d360c
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2026-08-30 15:06:25 +0300

    feat: add missing healthchecks

--- src/Laraue.Apps.RealEstate.ApiHost/Program.cs
+++ src/Laraue.Apps.RealEstate.ApiHost/Program.cs
@@ -14,6 +14,7 @@ var services = builder.Services;
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddOpenApi();
+services.AddHealthChecks();
 
 services.AddDbContext<AdvertisementsDbContext>(s =>
     s.UseNpgsql(builder.Configuration.GetConnectionString("Postgre"))
@@ -65,6 +66,7 @@ using var scope = app.Services.CreateScope();
 var dbContext = scope.ServiceProvider.GetRequiredService<AdvertisementsDbContext>();
 await dbContext.Database.MigrateAsync();
 
+app.MapHealthChecks("/_health");
 app.MapPrometheusScrapingEndpoint("/_metrics");
 
 app.Run();
